4041e381d19423b8bbd0c5b9b8f56c9176402fc1
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 17:04:30 +0100

    Return price after searching string for <$ (specific characters)

--- chapter2.py
+++ chapter2.py
@@ -2,7 +2,10 @@ import urllib.request
 
 page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
 text = page.read().decode("utf8")
-price = text[234:238]
+where = (text.find(">$"))
+start = where+2
+end = start+4
+price = text[start:end]
 output = 'Current cost of coffee is £' + str(price)
 print (output)
 
