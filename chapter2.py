57d33ae2f7089713c995f57ff26aa672406cd3ac
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 21:06:13 +0100

    Added 'while' statement to tell user to buy below a certain price

--- chapter2.py
+++ chapter2.py
@@ -1,11 +1,15 @@
-import urllib.request
 
-page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
-text = page.read().decode("utf8")
-where = (text.find(">$"))
-start = where+2
-end = start+4
-price = text[start:end]
-output = 'Current cost of coffee is £' + str(price)
+price = 99.9
+
+while price > 4.74:
+
+    import urllib.request
+    page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
+    text = page.read().decode("utf8")
+    where = (text.find(">$"))
+    start = where+2
+    end = start+4
+    price = text[start:end]
+
+output = 'Current cost of coffee is £' + str(price) + ' - you should buy it now!'
 print (output)
-
