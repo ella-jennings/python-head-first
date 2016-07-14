260b7530c8e1bc87279f05423a13132bda276fe8
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 16:38:15 +0100

    Added variable as string to printed text

--- chapter2.py
+++ chapter2.py
@@ -3,4 +3,6 @@ import urllib.request
 page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices.html")
 text = page.read().decode("utf8")
 price = text[234:238]
-print (price)
+output = 'Current cost of coffee is £' + str(price)
+print (output)
+
