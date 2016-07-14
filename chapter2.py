4262238a4f0cbd03e7acc82e0a8fcf76ee443c81
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 21:37:55 +0100

    Unsure if this would run a request straight off and then wait 15 minutes? Previous version would wait 15 minutes before the first request

--- chapter2.py
+++ chapter2.py
@@ -1,7 +1,12 @@
 import urllib.request
 import time
 
-price = 99.9
+page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
+text = page.read().decode("utf8")
+where = (text.find(">$"))
+start = where+2
+end = start+4
+price = float(text[start:end])
 
 while price > 4.74:
     time.sleep(900)
