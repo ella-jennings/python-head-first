90a4b8b748aa84fc2d3cd1b0bc2d7224db9dcdea
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 16:49:09 +0100

    Changed website to loyalty prices - string returns 'bean'

--- chapter2.py
+++ chapter2.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices.html")
+page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
 text = page.read().decode("utf8")
 price = text[234:238]
 output = 'Current cost of coffee is £' + str(price)
