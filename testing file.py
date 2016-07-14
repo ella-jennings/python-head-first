459bcd9df9f0c93fa7409539e425c3e2f3620676
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 22:34:55 +0100

    Getting original price request to work as a defined function, returned to main code and float from there

--- testing file.py
+++ testing file.py
@@ -1,10 +1,11 @@
-def bean_price():
+import urllib.request
+
+def get_price():
     page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
     text = page.read().decode("utf8")
     where = (text.find(">$"))
     start = where + 2
     end = start + 4
-    price = float(text[start:end])
+    print(text[start:end])
 
-price = 99.9
-bean_price()
+get_price()
