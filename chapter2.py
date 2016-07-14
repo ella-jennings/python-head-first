dbeabb6298499c5a5de669397b1e6b0e62a76435
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-07-14 23:13:04 +0100

    Created Y/N user option with input and 'if' statement to either run straight away and display actual price, or delay by 5 seconds and not print price until below required level

--- chapter2.py
+++ chapter2.py
@@ -1,21 +1,28 @@
 import urllib.request
 import time
 
-page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
-text = page.read().decode("utf8")
-where = (text.find(">$"))
-start = where+2
-end = start+4
-price = float(text[start:end])
-
-while price > 4.74:
-    time.sleep(900)
+def get_price():
     page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
     text = page.read().decode("utf8")
     where = (text.find(">$"))
-    start = where+2
-    end = start+4
-    price = float(text[start:end])
+    start = where + 2
+    end = start + 4
+    return(text[start:end])
+
+print("Welcome to the coffee bean pricing system.")
+choice = input("Do you require a price immediately? (Y/N)?")
+
+if choice == "Y":
+    price = float(get_price())
+
+
+if choice == "N":
+    price = float(get_price())
+    while price > 4.74:
+        print ("price is too high, checking in 5s")
+        time.sleep(5)
+        get_price()
+        price = float(get_price())
 
 output = 'Current cost of coffee is £' + str(price) + ' - you should buy it now!'
 print (output)
