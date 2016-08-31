97f97276f3e66d01c21117949b901c5740f23db9
Ella <ellajennings@ellas-mbp.home>
2016-08-31 17:38:50 +0100

    Used sorted() function to arrange scores, reversed to display high to low

--- Chapter 4.py
+++ Chapter 4.py
@@ -1,4 +1,5 @@
 scores = {}
+#created a hash
 result_f = open("results.txt")
 for line in result_f:
     (name, score) = line.split()
@@ -6,5 +7,5 @@ for line in result_f:
 result_f.close()
 
 print("The top scores were:")
-for each_score in scores.keys():
+for each_score in sorted(scores.keys(), reverse = True):
     print('Surfer ' + scores[each_score] + ' scored ' + each_score)
