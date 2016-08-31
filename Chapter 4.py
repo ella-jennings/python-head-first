d006460286311d8d14e836b2d83a69d0c43774ea
Ella <ellajennings@ellas-mbp.home>
2016-08-31 17:32:23 +0100

    Changed file from array to hash in order to create an association between name and score

--- Chapter 4.py
+++ Chapter 4.py
@@ -1,17 +1,10 @@
-scores = []
-names = []
-
+scores = {}
 result_f = open("results.txt")
 for line in result_f:
     (name, score) = line.split()
-    scores.append(float(score))
-    names.append(name)
+    scores[score] = name
 result_f.close()
 
-scores.sort(reverse = True)
-names.sort(reverse = True)
-
 print("The top scores were:")
-print(names[0] + ' with ' + str(scores[0]))
-print(names[1] + ' with ' + str(scores[1]))
-print(names[2] + ' with ' + str(scores[2]))
+for each_score in scores.keys():
+    print('Surfer ' + scores[each_score] + ' scored ' + each_score)
