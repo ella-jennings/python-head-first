d86a2b401d4de955d3ae9fd66ba630a55e4b3b69
Ella <ellajennings@ellas-mbp.home>
2016-08-31 22:14:09 +0100

    Tester code added, initial code for hash of line deleted

--- Chapter 5.py
+++ Chapter 5.py
@@ -1,17 +1,3 @@
-line = "101; Johnny 'wave-boy' Jones; USA; 8.32; Fish; 21"
-s = {}
-
-#split hash into parts every time a semicolon is seen
-(s['ID'], s['name'], s['country'], s['average'], s['board'], s['age']) = line.split(";")
-
-#display in the format below
-print("ID: " + s['ID'])
-print("Name:    " + s['name'])
-print("Country: " + s['country'])
-print("Average: " + s['average'])
-print("Board:   " + s['board'])
-print("Age:     " + s['age'])
-
 def find_details(id2find):
     #open file to read data
     surfers_f = open("surfing_data.csv")
@@ -19,13 +5,24 @@ def find_details(id2find):
     for each_line in surfers_f:
         #hash starts empty
         s = {}
-        #line split assigns data to the hash using multiple assignment
+        #line split assigns data to the hash using multiple assignment every time semicolon is seen
         (s['ID'], s['name'], s['country'], s['average'], s['board'], s['age']) = each_line.split(";")
         #check if ID supplied as a parameter is the same as the one from the file
-        if id2find == int(s['id']):
+        if id2find == int(s['ID']):
             surfers_f.close()
             #a match will return the current hash
             return(s)
     surfers_f.close()
     #no match will return an empty hash
     return({})
+
+lookup_ID = int(input("Enter the ID of the surfer: "))
+surfer = find_details(lookup_ID)
+if surfer:
+    # display in the format below
+    print("ID: " + surfer['ID'])
+    print("Name:    " + surfer['name'])
+    print("Country: " + surfer['country'])
+    print("Average: " + surfer['average'])
+    print("Board:   " + surfer['board'])
+    print("Age:     " + surfer['age'])
