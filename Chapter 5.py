2a98c4f99d90e9234eca9b6d763d13e1652e97a6
Ella <ellajennings@ellas-mbp.home>
2016-08-31 22:07:30 +0100

    returning data structure as a hash from a function

--- Chapter 5.py
+++ Chapter 5.py
@@ -12,3 +12,20 @@ print("Average: " + s['average'])
 print("Board:   " + s['board'])
 print("Age:     " + s['age'])
 
+def find_details(id2find):
+    #open file to read data
+    surfers_f = open("surfing_data.csv")
+    #for loop to look through each line of file
+    for each_line in surfers_f:
+        #hash starts empty
+        s = {}
+        #line split assigns data to the hash using multiple assignment
+        (s['ID'], s['name'], s['country'], s['average'], s['board'], s['age']) = each_line.split(";")
+        #check if ID supplied as a parameter is the same as the one from the file
+        if id2find == int(s['id']):
+            surfers_f.close()
+            #a match will return the current hash
+            return(s)
+    surfers_f.close()
+    #no match will return an empty hash
+    return({})
