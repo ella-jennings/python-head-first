1d0b19dfd1080a96dcf54e84c593b890e50ae83b
Ella <ellajennings@ellas-mbp.home>
2016-08-31 22:00:53 +0100

    Removed errors from previous entry

--- Chapter 5.py
+++ Chapter 5.py
@@ -2,11 +2,13 @@ line = "101; Johnny 'wave-boy' Jones; USA; 8.32; Fish; 21"
 s = {}
 
 #split hash into parts every time a semicolon is seen
-(s['ID'], s['name'], s['average'], s['board'], s['age'], =line.split(";")
+(s['ID'], s['name'], s['country'], s['average'], s['board'], s['age']) = line.split(";")
 
 #display in the format below
-print("ID: " + s['ID']
-print("Name: " + s['name']
-print("Average: " + s['average']
-print("Board: " + s['board']
-print("Age: " + s['age']
+print("ID: " + s['ID'])
+print("Name:    " + s['name'])
+print("Country: " + s['country'])
+print("Average: " + s['average'])
+print("Board:   " + s['board'])
+print("Age:     " + s['age'])
+
