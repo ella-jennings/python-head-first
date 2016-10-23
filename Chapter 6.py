cf437c28e5e8bb3d1e0719c624da0ad4bb01b418
Ella <ellajennings@Ellas-MacBook-Pro.local>
2016-10-23 15:39:50 +0100

    Applied a further discount if storecard option applied

--- Chapter 6.py
+++ Chapter 6.py
@@ -1,22 +1,29 @@
 # saved other code as a separate module (transactions.py) to import
-
+import promotion
+import storecard
 from transactions import *
 from promotion import *
+from storecard import *
 
 items = ["DONUT", "LATTE", "FILTER", "MUFFIN"]
 prices = [1.5, 2.0, 1.80, 1.20]
 running = True
 
 while running:
-    option = 1
-    for choice in items:
-        print(str(option) + ". " + choice)
-        option = option+1
-    print(str(option) + ". Quit")
+    option_number = 1
+    for item in items:
+        print(str(option_number) + ". " + item)
+        option_number += 1
+
+    print(str(option_number) + ". Quit")
+    end_program_option = option_number
     choice = int(input("Choose an option: "))
-    if choice == option:
+    if choice == end_program_option:
         running = False
     else:
+        store_card = input("Do you have a store card? Y/N: ")
         credit_card = input("Credit card number: ")
-        new_price = discount(prices[choice - 1])
-        save_transaction(new_price], credit_card, items[choice - 1])
+        new_price = promotion.discount(prices[choice - 1])
+        if store_card == "Y":
+            new_price = storecard.discount(new_price)
+        save_transaction(new_price, credit_card, items[choice - 1])
