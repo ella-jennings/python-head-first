49cf9dcbc7b949dc1332209212b02ad2f005d22c
Ella <erj503@gmail.com>
2017-02-05 12:14:31 +0000

    Changed filename

--- Chapter 2.py
+++ Chapter 2.py
@@ -1,6 +1,8 @@
 import urllib.request
 import time
 
+# LIBRARY
+
 def get_price():
     page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
     text = page.read().decode("utf8")
@@ -9,6 +11,19 @@ def get_price():
     end = start + 4
     return(text[start:end])
 
+def send_to_twitter(msg):
+    import sys
+    import tweepy
+    CONSUMER_KEY = '...'
+    CONSUMER_SECRET = '...'
+    ACCESS_KEY = '...'
+    ACCESS_SECRET = '...'
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    api.update_status(msg)
+
+# ACTUAL CODE
 print("Welcome to the coffee bean pricing system.")
 choice = input("Do you require a price immediately? (Y/N)?")
 
@@ -24,5 +39,9 @@ if choice == "N":
         get_price()
         price = float(get_price())
 
+
+
 output = 'Current cost of coffee is £' + str(price) + ' - you should buy it now!'
-print (output)
+
+send_to_twitter(output)
+
